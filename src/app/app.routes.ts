578fdfc52caefc234856e0395e46cb89492b1b84
Ahmed Gaber <ahmedmeagabr@gmail.com>
2025-11-16 22:12:15 +0200

    Done the Electric Bus Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ElectricBusComponent } from './electric-bus/electric-bus';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'electric-bus', component: ElectricBusComponent },
+    { path: '', redirectTo: '/electric-bus', pathMatch: 'full' }
+];
