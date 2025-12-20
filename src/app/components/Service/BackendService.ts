58864f28521d7c9781696159c91d4b070e596220
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-12-20 18:19:54 +0200

    ok

--- src/app/components/Service/BackendService.ts
+++ src/app/components/Service/BackendService.ts
@@ -1,15 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { inject } from "@angular/core";
-import { Client } from "./ClientIF"; 
+import { Client } from "./ClientIF";
 
 export class BackService{
-    baseUrl = "http://127.0.0.1:8080/Clients";
+    baseUrl = "http://127.0.0.1:8080/api/v1/auth";
     http = inject(HttpClient);
     CreateClient(Client : Client){
-        return this.http.post(this.baseUrl,Client)
+        return this.http.post(this.baseUrl + "/register",Client)
     }
     VerifySignIn(Client:Client){
-        const Url = this.baseUrl + "/Verify";
+        const Url = this.baseUrl + "/authenticate";
         return this.http.post(Url,Client);
     }
 }
