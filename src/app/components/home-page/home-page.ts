e355f5aaf9a8d0cd9003a54b7787c108da85e3ed
Ahmed Gaber <ahmedmeagabr@gmail.com>
2025-12-20 04:47:35 +0200

    feat: Adding Complaint

--- src/app/components/home-page/home-page.ts
+++ src/app/components/home-page/home-page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LucideAngularModule, User, LogOut } from "lucide-angular";
+import { LucideAngularModule, User, LogOut , FileWarning, X } from "lucide-angular";
 import { ElectricTrain } from '../../tabs/electric-train/electric-train';
 import { ElectricBusComponent } from "../electric-bus/electric-bus";
 import { Tram } from "../tram/tram";
 import { ActivatedRoute, Router } from '@angular/router';
-import { Client } from '../Service/ClientIF';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule, LucideAngularModule, ElectricTrain, ElectricBusComponent, Tram],
+  imports: [CommonModule, LucideAngularModule, ElectricTrain, ElectricBusComponent, Tram,FormsModule],
   templateUrl: './home-page.html',
   styleUrls: ['./home-page.css'],
   standalone: true,
@@ -18,8 +19,16 @@ export class HomePage {
   UserName: string | null = "User";
   readonly User = User;
   readonly LogOutIcon = LogOut;
-  tab = signal<string>("Electric Bus"); // the selected tab
+  tab = signal<string>("Electric Bus"); 
   tabs = ["Electric Bus", "Tram", "Train"];
+
+  complaintData = {
+    title: '',
+    description: '',
+    type: 'Service Issue',
+    ticketId: 'TCK-UTW4G0OMU',
+    severity: 'Medium'
+  };
 
   constructor(private router: Router, private ActiveRoute: ActivatedRoute) {
     const loggeddata = localStorage.getItem("Username")
@@ -35,12 +44,24 @@ export class HomePage {
   //   )
   // }
 
+  isModalOpen = signal<boolean>(false);
+
+  readonly FileWarning = FileWarning; 
+  readonly XIcon = X;
+
   // Add logout method
   onLogout() {
     this.router.navigate(['/sign-in']);
     localStorage.removeItem("Username")
     localStorage.removeItem("Email")
     localStorage.removeItem("TeleNumber")
+  }
+
+
+  onSubmitComplaint() {
+    console.log("Sending Data to Backend:", this.complaintData);
+    // 2l mafrod hena 2ana h3mel call ba2a lel service w 2deha 2l complaintData 3shan ttsagel fe 2l backend
+    this.isModalOpen.set(false);
   }
 }
 
